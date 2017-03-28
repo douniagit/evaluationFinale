c41eafca809322e4c1b9807cfe46ea6e99970ddb
dounia <d.fergane@gmail.com>
2017-03-29 00:01:47 +0200

    start onclick for articles

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -3,22 +3,41 @@ import articles from './../data/articles.js';
 import './Card.css';
 
 class Card extends React.Component{
-  render(){
-    return(
-      <div className="global">
-        <div className="cardContainer">
-            <p>Les articles du petit boudoir</p>
-            {articles.data.map(article=>{
+
+  constructor(props){
+    super(props);
+    this.state={
+      
+    }
+    
+  }
+
+
+  handleClick(article){
+      return this.globalMap();
+
+  }
+
+  globalMap(){
+    return articles.data.map(article=>{
               return(
               <div className="cards">
                 <img className="img" src={article.images} alt="img"/>
                 <h2 className="title">{article.name}</h2>
                 <p className="desc">{article.description}</p>
               </div>)
-            })}
+           })
+  }
+
+  render(){
+    return(
+      <div className="global">
+        <div className="cardContainer">
+            <p>Les articles du petit boudoir</p>
+            {}
         </div>
         <div className="bouton-article">
-          <button>TOUS LES ARTICLES</button>
+          <button onClick={this.handleClick.bind(this,'article')}>TOUS LES ARTICLES</button>
         </div>
       </div>
     )
